fcb520e0fd646337efe14cd2700af46ee2e61cae
Josh Imbriani <joshimbriani@gmail.com>
2020-01-06 07:39:41 -0800

    Adding leftover stuff

---
+++ 10-node/10-examples/reader.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+
+console.log("About to Read File");
+
+fs.readFile('about.txt', function(err, data) {
+	if (err) {
+		console.log("Unable to read file test.txt");
+	} else {
+		console.log("File Contents:");
+      	console.log(data.toString());
+	}
+});
+
+console.log("Called fs.readFile()\n");
